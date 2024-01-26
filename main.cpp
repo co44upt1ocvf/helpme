08b0397850ce9853f3fd7cd8526672dceb93cf69
co44upt1ocvf <corruold@gmail.com>
2024-01-26 15:15:57 +0300

    s pochinom

--- main.cpp
+++ main.cpp
@@ -4,17 +4,17 @@
 
 using namespace std;
 
-int factorial(int n){
+int factorial(double n) {
     int result = 1;
     for (int i = 1; i <= n; i++) {
-        result  *= i;
+        result *= i;
     }
     return result;
 }
 
 int main() {
     system("chcp 65001");
-    double a, b;
+    double a, b = 0.0;
     int choice;
     do {
         cout << "Выберите операцию:\n"
@@ -28,50 +28,200 @@ int main() {
                 "\t8. Найти факториал числа\n"
                 "\tЕсли вы хотите выйти нажмите - \"9\"\n"
                 "Операция: ";
-        cin >> choice;
-
-        if (choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5 || choice == 7){
-            cout << "Введите первое число: ";
-            cin >> a;
-            cout << "Введите второе число: ";
-            cin >> b;
+        if (cin >> choice)
+        {
+            switch (choice) {
+                case 1:
+                    cout << "Введите первое число: ";
+                    if (cin >> a)
+                    {
+                        cout << "Введите второе число: ";
+                        if (cin >> b)
+                        {
+                            cout << "Результат: " << a + b << endl;
+                        }
+                        else
+                        {
+                            cout << "Введено НЕ число!\n" << endl;
+                            cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+                            getch();
+                            exit(0);
+                        }
+                    }
+                    else
+                    {
+                        cout << "Введено НЕ число!\n" << endl;
+                        cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+                        getch();
+                        exit(0);
+                    }
+                    break;
+                case 2:
+                    cout << "Введите первое число: ";
+                    if (cin >> a)
+                    {
+                        cout << "Введите второе число: ";
+                        if (cin >> b)
+                        {
+                            cout << "Результат: " << a - b << endl;
+                        }
+                        else
+                        {
+                            cout << "Введено НЕ число!\n" << endl;
+                            cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+                            getch();
+                            exit(0);
+                        }
+                    }
+                    else
+                    {
+                        cout << "Введено НЕ число!\n" << endl;
+                        cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+                        getch();
+                        exit(0);
+                    }
+                    break;
+                case 3:
+                    cout << "Введите первое число: ";
+                    if (cin >> a)
+                    {
+                        cout << "Введите второе число: ";
+                        if (cin >> b)
+                        {
+                            cout << "Результат: " << a * b << endl;
+                        }
+                        else
+                        {
+                            cout << "Введено НЕ число!\n" << endl;
+                            cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+                            getch();
+                            exit(0);
+                        }
+                    }
+                    else
+                    {
+                        cout << "Введено НЕ число!\n" << endl;
+                        cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+                        getch();
+                        exit(0);
+                    }
+                    break;
+                case 4:
+                    cout << "Введите первое число: ";
+                    if (cin >> a)
+                    {
+                        cout << "Введите второе число: ";
+                        if (cin >> b)
+                        {
+                            if (b == 0) {
+                                cout << "Нельзя делить на ноль!!!\n";
+                            }
+                            else {
+                                cout << a / b << endl;
+                            }
+                        }
+                        else
+                        {
+                            cout << "Введено НЕ число!\n" << endl;
+                            cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+                            getch();
+                            exit(0);
+                        }
+                    }
+                    else
+                    {
+                        cout << "Введено НЕ число!\n" << endl;
+                        cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+                        getch();
+                        exit(0);
+                    }
+                    break;
+                case 5:
+                    cout << "Введите первое число: ";
+                    if (cin >> a)
+                    {
+                        cout << "Введите второе число: ";
+                        if (cin >> b)
+                        {
+                            cout << "Результат: " << pow(a, b) << endl;
+                        }
+                        else
+                        {
+                            cout << "Введено НЕ число!\n" << endl;
+                            cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+                            getch();
+                            exit(0);
+                        }
+                    }
+                    else
+                    {
+                        cout << "Введено НЕ число!\n" << endl;
+                        cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+                        getch();
+                        exit(0);
+                    }
+                    break;
+                case 6:
+                    cout << "Введите число: ";
+                    if (cin >> a)
+                    {
+                        cout << sqrt(a) << endl;
+                    }
+                    else {
+                        cout << "Введено НЕ число!\n" << endl;
+                        cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+                        getch();
+                        exit(0);
+                    }
+                    break;
+                case 7:
+                    cout << "Введите первое число: ";
+                    if (cin >> a)
+                    {
+                        cout << "Введите второе число: ";
+                        if (cin >> b)
+                        {
+                            cout << "Результат: " << ((a / 100) * b) << "%" << endl;
+                        }
+                        else
+                        {
+                            cout << "Введено НЕ число!\n" << endl;
+                            cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+                            getch();
+                            exit(0);
+                        }
+                    }
+                    else
+                    {
+                        cout << "Введено НЕ число!\n" << endl;
+                        cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+                        getch();
+                        exit(0);
+                    }
+                    break;
+                case 8:
+                    cout << "Введите число: ";
+                    if (cin >> a)
+                    {
+                        cout << factorial(a) << "\n";
+                    }
+                    else {
+                        cout << "Введено НЕ число!\n" << endl;
+                        cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+                        getch();
+                        exit(0);
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
-        else if (choice == 6 || choice == 8){
-            cout << "Введите число: ";
-            cin >> a;
-        }
-
-        switch (choice) {
-            case 1:
-                cout << a + b << "\n";
-                break;
-            case 2:
-                cout << a - b << "\n";
-                break;
-            case 3:
-                cout << a * b << "\n";
-                break;
-            case 4:
-                if (b == 0) {
-                    cout << "Нельзя делить на ноль!!!\n";
-                } else {
-                    cout << a / b << "\n";
-                }
-                break;
-            case 5:
-                cout << pow(a,b) << "\n";
-                break;
-            case 6:
-                cout << sqrt(a) << "\n";
-                break;
-            case 7:
-                cout << ((a /100) * b) << "%" << "\n";
-                break;
-            case 8:
-                cout << factorial(a) << "\n";
-                break;
-            default:
-                break;
+        else
+        {
+            cout << "Введено НЕ число!\n" << endl;
+            cout << "Нажмите на любую клавишу, чтобы закрыть окно!" << endl;
+            getch();
+            exit(0);
         }
     } while (choice != 9);
 
